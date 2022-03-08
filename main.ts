526c439823b3f9f0e767d48e9e4633072ae325d2
kittenbothk <kittenbotdummy@gmail.com>
2022-03-08 10:48:32 +0800

    init

--- main.ts
+++ main.ts
@@ -1,17 +1,28 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-let x = ""
-serial.redirect(
-SerialPin.P2,
-SerialPin.P12,
-BaudRate.BaudRate115200
-)
-basic.forever(function () {
-    x = serial.readString()
-    if (x == "asr200") {
-        Sugar.ledOnoff(DigitalPin.P1, Sugar.LEDSta.On)
-    } else if (x == "asr201") {
-        Sugar.ledOnoff(DigitalPin.P1, Sugar.LEDSta.Off)
-    }
-})
+//% color="#5c7cfa" weight=10 icon="\u03f0"
+//% groups='["Basic", "Graphic", Classifier", "Tag/Code", "Audio", "Face", "Wifi", "CloudAI"]'
+namespace koi {
+
+serial.onDataReceived('\n', function () {
+    let a = serial.readUntil('\n')
+    display.scroll(a)
+  })
+}
+
+/**
+   * init serial port
+   * @param tx Tx pin; eg: SerialPin.P1
+   * @param rx Rx pin; eg: SerialPin.P2
+   */
+  //% blockId=koi_init block="ASR init|Tx pin %tx|Rx pin %rx"
+  //% group="Basic" weight=100
+  export function asr_init(tx: SerialPin, rx: SerialPin): void {
+    serial.redirect(tx, rx, BaudRate.BaudRate115200)
+    basic.pause(100)
+    //serial.setTxBufferSize(64)
+    //serial.setRxBufferSize(64)
+    //serial.readString()
+    //serial.writeString('\n\n')
+    // take control of the ext serial port from KOI
+    //asyncWrite(`K0`, 0)
+    basic.pause(300)
+  }

d4a34c8a17c8885a0655074724ca608676991ab0
kittenbothk <kittenbotdummy@gmail.com>
2022-05-05 13:16:45 +0800

    cpp union

--- test.cpp
+++ test.cpp
@@ -2,11 +2,16 @@
 
 namespace test {
 
-char convert(double num){
-union {
-  double d[num];
-  char b[sizeof(double) * 2];
-};
-return b;
+void convert(double num){
+union{
+	double myDouble;
+	unsigned char myChars[sizeof(double)];
+	} test;
+
+	test.myDouble = num;
+	for( int k = 0; k < sizeof(double); k++ ){
+	    //std::cout << (int)test.myChars[k] << ' ';
+        continue;
+	}
 }
 }

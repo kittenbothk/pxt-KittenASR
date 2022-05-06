b675679cd755b7944678a0bda97f6e54d6d7d5b1
kittenbothk <kittenbotdummy@gmail.com>
2022-05-06 09:59:42 +0800

    aaa

--- test.cpp
+++ test.cpp
@@ -17,43 +17,7 @@ string current="";
             current = current.append(" ");
 	}
 	*/
-	return (string)current;
+	return current;
 }
 
-
-/*
-int convert(double num){
-union{
-	double myDouble;
-	unsigned char myChars[sizeof(double)];
-	} test;
-
-	test.myDouble = num;
-	for( int k = 0; k < sizeof(double); k++ ){
-	    //std::cout << (int)test.myChars[k] << ' ';
-        continue;
-	}
-	return (int)test.myChars;
-}
-*/
-/*
-int* convert(double num){
-union{
-	double myDouble;
-	unsigned char myChars[sizeof(double)];
-	} test;
-	test.myDouble = num;
-	int results[sizeof(double)];
-	for( int k = 0; k < sizeof(double); k++ ){
-	    results[k]=(int)test.myChars[k];
-	}
-	int *ptr=results;
-	return ptr;
-}
-
-int getArray(double input){
-    int* ans=convert(input);
-    return ans;
-}
-*/
 }
